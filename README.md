6b0ef89d22877621fdb7ed899985d8002c01e16a
cmondragon <cmondragon@vultr.com>
2025-03-11 14:47:57 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# rancher-ui-extensions-vultr
-Vultr Rancher UI extension
+# Vultr Rancher UI extension
+
+
+Rancher 2.10 UI driver for [Vultr](https://www.vultr.com).
---
+++ vue.config.js
@@ -0,0 +1,6 @@
+const config = require('@rancher/shell/vue.config'); // eslint-disable-line @typescript-eslint/no-var-requires
+const configHelper = require('@rancher/shell/vue-config-helper.js');
+
+module.exports = config(__dirname, {
+  excludes: [],
+});
